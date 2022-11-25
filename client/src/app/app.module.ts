c72af3c17f99358a96b2644250dcfddafa402c08
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-25 19:32:03 +0200

    Added home component to declarations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ViewsModule } from './views/views.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        CoreModule,
-        ViewsModule
+        CoreModule
     ],
     providers: [],
     bootstrap: [AppComponent]
