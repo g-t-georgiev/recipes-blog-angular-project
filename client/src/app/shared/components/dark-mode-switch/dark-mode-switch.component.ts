1451c94e0770015fbdc284056657b6feef17f52d
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-25 20:17:42 +0200

    Created dark mode switch component

---
+++ client/src/app/shared/components/dark-mode-switch/dark-mode-switch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-dark-mode-switch',
+	templateUrl: './dark-mode-switch.component.html',
+	styleUrls: ['./dark-mode-switch.component.css']
+})
+export class DarkModeSwitchComponent {
+
+}
