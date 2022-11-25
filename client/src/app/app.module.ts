68d656f2b7af403d10ed02db6294fe6ea0cbbd74
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-25 20:16:55 +0200

    Added shared module to imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        SharedModule
     ],
     providers: [],
     bootstrap: [AppComponent]
