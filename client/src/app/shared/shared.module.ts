97144597513ba3d71a710a149cdbfd9755613e43
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-25 20:22:21 +0200

    Added forms module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DarkModeSwitchComponent } from './components/dark-mode-switch/dark-mode-switch.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { DarkModeSwitchComponent } from './components/dark-mode-switch/dark-mode
 		DarkModeSwitchComponent
 	],
 	imports: [
-		CommonModule
+		CommonModule,
+		FormsModule
 	],
 	exports: [
 		DarkModeSwitchComponent
