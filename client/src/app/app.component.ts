0cd9c8a129e15d6fc02ac041eba47bc1d91e6a74
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 18:50:38 +0200

    Subscribed for retriving current dark mode flag value and set logic for toggling theme--dark class on body element

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DarkModeSwitchService } from './core/services/dark-mode-switch.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+    private darkModeToggleSubscription!: Subscription;
+
+    constructor(
+        private themeService: DarkModeSwitchService
+    ) { }
+
+    ngOnInit(): void {
+
+        this.darkModeToggleSubscription = this.themeService.isDarkModeOn$.subscribe({
+            next: (darkModeOn: boolean): void => {
+                this.themeService.toggleThemeStyle(darkModeOn);
+            }
+        });
+
+    }
+
+    ngOnDestroy(): void {
+        this.darkModeToggleSubscription.unsubscribe?.();
+    }
+
+}
