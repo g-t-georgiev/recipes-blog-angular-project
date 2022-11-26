155837f5f0816a8b7dcb4ebfbc28c3d472b8a961
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 17:57:00 +0200

    Created globalThis/window object provider via InjectionToken

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DarkModeSwitchComponent } from './components/dark-mode-switch/dark-mode-switch.component';
 import { FormsModule } from '@angular/forms';
+import { WINDOW } from './custom-di-tokens';
 
 
 
@@ -15,6 +16,13 @@ import { FormsModule } from '@angular/forms';
 	],
 	exports: [
 		DarkModeSwitchComponent
+	],
+	providers: [
+		{
+			provide: WINDOW,
+			useValue: window ?? globalThis,
+			multi: true 
+		}
 	]
 })
 export class SharedModule { }
