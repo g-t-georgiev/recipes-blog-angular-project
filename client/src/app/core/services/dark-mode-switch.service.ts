53062d5c2355b844d4e69f21166ee1fbe0c8751d
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 18:42:49 +0200

    Added methods for toggling dark mode flag and switch styles

--- client/src/app/core/services/dark-mode-switch.service.ts
+++ client/src/app/core/services/dark-mode-switch.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { WINDOW } from 'src/app/shared/custom-di-tokens';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IRootState, selectDarkModeFeatureValue, toggleDarkMode } from 'src/app/+state';
 
 @Injectable()
 export class DarkModeSwitchService {
 
-	constructor() { }
+	isDarkModeOn$!: Observable<boolean>;
+
+	constructor(
+		@Inject(DOCUMENT) private document: Document,
+		@Inject(WINDOW) private window: Window | typeof globalThis,
+		private state: Store<IRootState>
+	) {
+		this.isDarkModeOn$ = this.state.select(selectDarkModeFeatureValue);
+	}
+
+	switchDarkMode(darkModeOn: boolean): void {
+		this.state.dispatch(toggleDarkMode({ darkModeOn }));
+	}
+
+	toggleThemeStyle(darkModeOn: boolean): void {
+		this.document.body.classList.toggle('theme--dark', darkModeOn);
+	}
 
 }
