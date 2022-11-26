bca4dd78f508da8f54a0e3b88018ecbc49e77c6c
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 21:35:56 +0200

    Added default value from factory for DI token and set prodidedIn property to root injector explicitly

--- client/src/app/shared/custom-di-tokens.ts
+++ client/src/app/shared/custom-di-tokens.ts
@@ -1,3 +1,8 @@
 import { InjectionToken } from '@angular/core';
 
-export const WINDOW = new InjectionToken<typeof globalThis | Window>('Global Object DI Token');
+export const WINDOW = new InjectionToken<typeof globalThis & Window>(
+    'Global Object DI Token', 
+    { 
+        providedIn: 'root', 
+        factory: () => window || globalThis 
+    });
