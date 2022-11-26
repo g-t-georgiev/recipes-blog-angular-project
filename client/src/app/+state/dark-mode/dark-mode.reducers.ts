59724b33e08b734ff34bd32f5e52b92148d362a1
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 21:43:11 +0200

    Added comments for debugging

--- client/src/app/+state/dark-mode/dark-mode.reducers.ts
+++ client/src/app/+state/dark-mode/dark-mode.reducers.ts
@@ -1,9 +1,19 @@
 import { createReducer, on } from '@ngrx/store';
 import { toggleDarkMode } from './dark-mode.actions';
+// import { IDarkModeFeature } from './dark-mode.selectors';
 
 const initialDarkModeState = false;
 
 export const darkModeReducer = createReducer<boolean>(
     initialDarkModeState,
-    on(toggleDarkMode, (state, action) => action.darkModeOn)
+    on(toggleDarkMode, (state, action) => {
+
+        // console.log(
+        //     'DarkModeStateReducer#onToggleDarkMode', 
+        //     `currentState: ${state} `, 
+        //     `newStateFromProps: ${action.darkModeOn}`
+        // );
+
+        return action.darkModeOn;
+    })
 );
