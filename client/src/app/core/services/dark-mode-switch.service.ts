8fd9c8f4de56fc26b9b0c1b2cdd470fc5864b1bd
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-26 21:43:54 +0200

    Added comments for debugging

--- client/src/app/core/services/dark-mode-switch.service.ts
+++ client/src/app/core/services/dark-mode-switch.service.ts
@@ -3,26 +3,40 @@ import { DOCUMENT } from '@angular/common';
 import { WINDOW } from 'src/app/shared/custom-di-tokens';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { IRootState, selectDarkModeFeatureValue, toggleDarkMode } from 'src/app/+state';
+import { IRootState, toggleDarkMode } from 'src/app/+state';
 
 @Injectable()
 export class DarkModeSwitchService {
 
-	isDarkModeOn$!: Observable<boolean>;
+	get preferColorSchemeDarkMediaQuery(): MediaQueryList {
+		return this.window.matchMedia('(prefers-color-scheme: dark)');
+	}
 
 	constructor(
 		@Inject(DOCUMENT) private document: Document,
-		@Inject(WINDOW) private window: Window | typeof globalThis,
+		@Inject(WINDOW) private window: Window & typeof globalThis,
 		private state: Store<IRootState>
-	) {
-		this.isDarkModeOn$ = this.state.select(selectDarkModeFeatureValue);
-	}
+	) { }
 
 	switchDarkMode(darkModeOn: boolean): void {
-		this.state.dispatch(toggleDarkMode({ darkModeOn }));
+
+		// console.log(
+		// 	'DarkModeSwitchService#switchDarkMode', 
+		// 	`newState: ${darkModeOn}`
+		// );
+
+		if (typeof darkModeOn === 'boolean') {
+			this.state.dispatch(toggleDarkMode({ darkModeOn }));
+		}
 	}
 
 	toggleThemeStyle(darkModeOn: boolean): void {
+
+		// console.log(
+		// 	'DarkModeSwitchService#toggleThemeStyle', 
+		// 	`toggleStyles: ${darkModeOn}`
+		// );
+
 		this.document.body.classList.toggle('theme--dark', darkModeOn);
 	}
 
