548216df4f3c41cf0ea5dca3e8e8ee1307ccfb7b
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-29 19:05:17 +0200

    Fetch environment variables directly from process.env

--- server/config/env.config.js
+++ server/config/env.config.js
@@ -1,21 +1,36 @@
-import * as appConfig from '../app.config.js';
+const { 
+    NODE_ENV,
+    API_PORT,
+    DB_URL_CREDENTIALS
+} = process.env;
+
+// if (NODE_ENV === 'development') {
+
+//     console.log(
+//         'EnvConfig#module',
+//         'NODE_ENV: ', NODE_ENV,
+//         'API_PORT: ', API_PORT,
+//         'DB_URL_CREDENTIALS: ', DB_URL_CREDENTIALS
+//     );
+
+// }
 
 const config = {
     development: {
-        port: appConfig.API_PORT,
-        dbURL: appConfig.DB_URL_CREDENTIALS,
+        port: API_PORT,
+        dbURL: DB_URL_CREDENTIALS,
         origin: [
             'http://localhost:5555', 
             'http://localhost:4200'
         ],
     },
     production: {
-        port: appConfig.API_PORT,
-        dbURL: appConfig.DB_URL_CREDENTIALS,
+        port: API_PORT,
+        dbURL: DB_URL_CREDENTIALS,
         origin: [],
     }
 };
 
 // console.log(config);
 
-export default config[appConfig.NODE_ENV];
+export default config[NODE_ENV];
