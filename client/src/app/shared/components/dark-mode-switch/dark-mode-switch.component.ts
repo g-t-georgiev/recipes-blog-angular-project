f5fb171addf06da8a58bd86aaf4d2d28750402cd
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-29 22:16:59 +0200

    Prepare for refactoring - deleted old code

--- client/src/app/shared/components/dark-mode-switch/dark-mode-switch.component.ts
+++ client/src/app/shared/components/dark-mode-switch/dark-mode-switch.component.ts
@@ -12,61 +12,27 @@ import { DarkModeSwitchService } from 'src/app/core/services/dark-mode-switch.se
 })
 export class DarkModeSwitchComponent implements OnInit, OnDestroy {
 
-	form!: FormGroup;
-
 	isDarkModeOn$!: Observable<boolean>;
 
-	private formValueChangesSubscription!: Subscription;
 	private darkModeToggleSubscription!: Subscription;
 
 	constructor(
 		private themeService: DarkModeSwitchService,
 		private state: Store<IRootState>
 	) {
+		
 		this.isDarkModeOn$ = this.state.select(globalState => globalState.darkModeOn);
-
-		this.form = new FormGroup({
-			darkModeOn: new FormControl(null)
-		});
-	
 	}
 
 	ngOnInit(): void {
 
-		this.darkModeToggleSubscription = this.isDarkModeOn$.subscribe({
-			next: (darkModeOn: boolean): void => {
-
-				// console.log(
-				// 	'DarkModeSwitchComponent#ngOninit', 
-				// 	`currentState: ${darkModeOn}`
-				// );
-				
-				if (this.form.get('darkModeOn')?.value !== darkModeOn) {
-
-					// console.log(
-					// 	'DarkModeSwitchComponent#ngOninit', 
-					// 	`currentFormControlValue: ${this.form.get('darkModeOn')?.value}`,
-					// 	`newFormControlValue: ${darkModeOn}`
-					// );
-
-					this.form.patchValue({ darkModeOn });
-				}
-				
-			}
-		});
-
-		this.formValueChangesSubscription = this.form.valueChanges.subscribe({
-			next: ({ darkModeOn }) => {
-				this.themeService.switchDarkMode(darkModeOn);
-			}
-		});
-		
+		this.darkModeToggleSubscription = this.isDarkModeOn$.subscribe();
 	}
 
 
 	ngOnDestroy(): void {
+
 		this.darkModeToggleSubscription.unsubscribe?.();
-		this.formValueChangesSubscription
 	}
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DarkModeSwitchComponent } from './components/dark-mode-switch/dark-mode-switch.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { WINDOW } from './custom-di-tokens';
 
 
 
 @NgModule({
 	declarations: [
-		DarkModeSwitchComponent
+		DarkModeSwitchComponent,
 	],
 	imports: [
 		CommonModule,
-		ReactiveFormsModule
 	],
 	exports: [
 		DarkModeSwitchComponent
