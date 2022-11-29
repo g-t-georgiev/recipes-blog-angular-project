4839e6209586a6540ccde8cb38c73c40e2de949c
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-11-29 19:06:05 +0200

    Fetch environment variables directly from process.env

--- server/config/express.config.js
+++ server/config/express.config.js
@@ -4,29 +4,47 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 import envConfig from './env.config.js';
-import * as appConfig from '../app.config.js';
 
 import { errorMiddleware } from '../middlewares/index.js';
-import apiRouter from '../router/index.js';
+import router from '../router/index.js';
 
+
+const { 
+    NODE_ENV,
+    AUTH_COOKIE_SECRET
+} = process.env;
+
+// if (NODE_ENV === 'development') {
+
+//     console.log(
+//         'ExpressConfig#module',
+//         'NODE_ENV: ', NODE_ENV,
+//         'AUTH_COOKIE_SECRET: ', AUTH_COOKIE_SECRET
+//     );
+
+// }
 
 const corsOptions = {
     origin: envConfig.origin,
     credentials: true
 }
 
-export default function(app) {
+/**
+ * Initialize express application with configuration options.
+ */
+export function expressAppInit() {
 
-    app.use(express.json());
-
-    app.use(cookieParser(appConfig.AUTH_COOKIE_SECRET));
-
-    app.use(express.static(path.resolve(__basedir, 'static')));
-
-    app.use(cors(corsOptions));
-  
-    app.use('/api', apiRouter);
-  
-    app.use(errorMiddleware);
+    const app = express(); // init app
+    app.use( express.json() ); // parse json
+    app.use( cookieParser( AUTH_COOKIE_SECRET ) ); // parse cookies
+    app.use( express.static( path.resolve( __basedir, 'static' ) ) ); // serve static files from 'static' directory
+    app.use( cors( corsOptions ) ); // pars cross-origin requests
+    app.use( '/api', router ); // setup router
+    app.use( errorMiddleware ); // parse errors
+    
+    return app;
 
 };
+
+
+export default expressAppInit;
