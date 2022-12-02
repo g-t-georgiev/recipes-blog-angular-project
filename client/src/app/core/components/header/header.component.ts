7dbce128278d2e0f92fb4f196ae8bb7642042769
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-02 18:31:57 +0200

    Added show/hide logic of burder menu icon on screen sizes below 780px

--- client/src/app/core/components/header/header.component.ts
+++ client/src/app/core/components/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+
+import { WINDOW } from 'src/app/shared/custom-di-tokens';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+	showMenuToggleBtn: boolean = false;
+
+	private get vpSizeChangeMQ(): MediaQueryList {
+		return this.window.matchMedia('(max-width: 780px)');
+	}
+
+	private vpSizeChangeHandler: (ev: MediaQueryListEvent) => void = (vpSizeChangeEv) => {
+		this.showMenuToggleBtn = vpSizeChangeEv.matches;
+	};
+
+	constructor(
+		@Inject(WINDOW) private window: typeof globalThis & Window
+	) { }
+
+	ngOnInit(): void {
+		this.vpSizeChangeMQ.addEventListener('change', this.vpSizeChangeHandler);
+	}
+
+	ngOnDestroy(): void {
+		this.vpSizeChangeMQ.removeEventListener('change', this.vpSizeChangeHandler);
+	}
 
 }
