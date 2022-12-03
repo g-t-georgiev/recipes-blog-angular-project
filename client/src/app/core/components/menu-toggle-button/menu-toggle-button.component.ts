bd6f56092805a0863e896601003ac1a951032608
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-03 18:05:57 +0200

    Added color, animation styling and behaviour of SVG icon, as well as component logic for changing and emitting its toggled state to the parent

--- client/src/app/core/components/menu-toggle-button/menu-toggle-button.component.ts
+++ client/src/app/core/components/menu-toggle-button/menu-toggle-button.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { 
+	Component, 
+	ViewChild, 
+	ElementRef, 
+	OnInit, 
+	OnChanges,
+	AfterViewInit, 
+	Input,
+	Output, 
+	EventEmitter, 
+	Renderer2, 
+	SimpleChanges
+} from '@angular/core';
+
+
+enum ToggleBtnState {
+	OPENED = 'opened',
+	CLOSED = 'closed'
+}
 
 @Component({
-  selector: 'app-menu-toggle-button',
-  templateUrl: './menu-toggle-button.component.html',
-  styleUrls: ['./menu-toggle-button.component.css']
+	selector: 'app-menu-toggle-button',
+	templateUrl: './menu-toggle-button.component.html',
+	styleUrls: ['./menu-toggle-button.component.css']
 })
-export class MenuToggleButtonComponent {
+export class MenuToggleButtonComponent implements OnInit, OnChanges, AfterViewInit {
+
+	private toggleState: boolean = false; 
+	private get btnTextValue(): ToggleBtnState {
+		return this.toggleState ? ToggleBtnState.OPENED : ToggleBtnState.CLOSED;
+	}
+
+	@Input('is-smallScreen') private isOnSmallScreen: boolean = false;
+	@Output('on-toggle') private onToggle: EventEmitter<boolean> = new EventEmitter();
+	@ViewChild('menuToggleBtn') private menuToggleBtn!: ElementRef<HTMLButtonElement>;
+
+	constructor(
+		private renderer: Renderer2
+	) { }
+
+	ngOnInit(): void {
+		// this.toggled = !this.isOnSmallScreen;
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log(changes);
+	}
+
+	ngAfterViewInit(): void {
+
+		if (this.menuToggleBtn && this.menuToggleBtn.nativeElement) {
+
+			this.renderer.setValue(
+				this.menuToggleBtn.nativeElement,
+				this.btnTextValue
+			);
+
+		}
+
+	}
+
+
+
+	toggleBtn(event: PointerEvent) {
+		this.toggleState = !this.toggleState;
+		
+		const button = event.currentTarget as HTMLButtonElement;
+		button.value = this.btnTextValue;
+		this.onToggle.emit(this.toggleState);
+	}
 
 }
