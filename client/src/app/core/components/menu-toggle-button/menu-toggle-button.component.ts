e4541316a96e8817ef5fddd2fd04def02f85ed41
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-03 14:56:08 +0200

    Created menu toggle button component

---
+++ client/src/app/core/components/menu-toggle-button/menu-toggle-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-toggle-button',
+  templateUrl: './menu-toggle-button.component.html',
+  styleUrls: ['./menu-toggle-button.component.css']
+})
+export class MenuToggleButtonComponent {
+
+}
