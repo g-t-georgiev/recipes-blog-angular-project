1802d855b38d4a608247e08b6ed552a9040e3b8a
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-05 20:50:11 +0200

    Subscribe for viewport resize changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 import { IRootState } from './+state';
 import { DarkModeSwitchService } from './core/services/dark-mode-switch.service';
+import { ViewportResizeService } from './core/services/viewport-resize.service';
 
 @Component({
     selector: 'app-root',
@@ -12,27 +14,22 @@ import { DarkModeSwitchService } from './core/services/dark-mode-switch.service'
 export class AppComponent implements OnInit {
 
     constructor(
-        private themeService: DarkModeSwitchService,
+        private themeService: DarkModeSwitchService, 
+        private resizeService: ViewportResizeService, 
         private state: Store<IRootState>
     ) { }
 
     ngOnInit(): void {
+        this.state.select(globalState => globalState.darkModeOn)
+            .pipe(
+                tap((darkModeOn) => {
+                    this.themeService.setStyles(darkModeOn);
+                })
+            )
+            .subscribe();
 
-        this.state.select(globalState => globalState.darkModeOn).subscribe({
-            next: (darkModeOn: boolean): void => {
-
-                // console.log(
-                //     'AppComponent#darkModeToggleSubscription', 
-                //     `currentState: ${darkModeOn}`
-                // );
-
-                this.themeService.setStyles(darkModeOn);
-            }
-        });
-
-        this.themeService.onChanges(
-            ({ matches: darkModeOn }) => this.themeService.setPreference(darkModeOn ? 'dark' : 'light')
-        );
+        this.resizeService.onMaxWidth780$.subscribe();
+        this.themeService.onColorSchemeDark$.subscribe();
 
     }
 
