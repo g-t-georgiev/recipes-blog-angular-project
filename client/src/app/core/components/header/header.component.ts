c6d389aa2fdb43be819be6dee7554ae8283ab0ec
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-06 20:44:47 +0200

    Refactored logic for handling navigation and header expansion, toggle button hide/show state and header area clicks

--- client/src/app/core/components/header/header.component.ts
+++ client/src/app/core/components/header/header.component.ts
@@ -5,11 +5,22 @@ import {
 	HostBinding,  
 } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { 
+	Subscription, 
+	BehaviorSubject, 
+	Observable, 
+} from 'rxjs';
 
 import { ViewportResizeService } from '../../services/viewport-resize.service';
 
 
+
+interface ILocalState {
+	toggleNavigation: boolean,
+	showToggleBtn: boolean,
+	expandHeader: boolean, 
+	forceClose: boolean
+}
 
 @Component({
 	selector: 'app-header',
@@ -20,22 +31,35 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
 	private subscription!: Subscription;
 
-	isNavCollapsed: boolean = false;
+	private _localState$: BehaviorSubject<ILocalState> = new BehaviorSubject<ILocalState>({
+		toggleNavigation: true,
+		showToggleBtn: false,
+		expandHeader: false, 
+		forceClose: false
+	});
 
-	@HostBinding('class.fullscreen') expand: boolean = false;
+	localState$: Observable<ILocalState> = this._localState$.asObservable();
 
 	constructor(
-		private resizeService: ViewportResizeService
+		private vpResizeService: ViewportResizeService
 	) { }
 
 	ngOnInit(): void {
-		this.isNavCollapsed = !this.resizeService.hasMatch();
-		this.expand = this.resizeService.hasMatch() && this.isNavCollapsed;
+		this.setState(
+			!this.vpResizeService.hasMatch(), // hide navigation on match
+			this.vpResizeService.hasMatch(), // show toggle nav button on match
+			false, // do not expand header on fullscreen by default
+			false // do not force close navigation by default
+		);
 
-		this.subscription = this.resizeService.onMaxWidth780$.subscribe({
+		this.subscription = this.vpResizeService.onMaxWidth780$.subscribe({
 			next: ({ matches }) => {
-				this.isNavCollapsed = !matches;
-				this.expand = matches && this.isNavCollapsed;
+				this.setState(
+					!matches, // hide navigation on match
+					matches, // show toggle nav button on match
+					false, // do not expand header on fullscreen by default
+					false // do not force close navigation by default
+				);
 			}
 		});
 	}
@@ -44,8 +68,45 @@ export class HeaderComponent implements OnInit, OnDestroy {
 		this.subscription?.unsubscribe();
 	}
 
+	private setState(toggleNavigation: boolean, showToggleBtn: boolean, expandHeader: boolean, forceClose: boolean) {
+		this._localState$.next({
+			toggleNavigation,
+			showToggleBtn,
+			expandHeader,
+			forceClose
+		});
+	}
+
 	onMenuButtonToggle(menuBtnToggled: boolean) {
-		this.isNavCollapsed = menuBtnToggled;
-		this.expand = this.isNavCollapsed;
+		this.setState(
+			menuBtnToggled, // hide navigation on toggle
+			true, // show togle button by default
+			menuBtnToggled, // expand header fullscreen on toggle
+			false // do not force close navigation by default
+		);
+	}
+
+	onHeaderAreaClick(ev: PointerEvent, isHeaderExpanded: boolean) {
+		if (!isHeaderExpanded) { return; }
+
+		const tagName = (ev.target as HTMLElement).tagName;
+
+		// console.log(tagName);
+
+		if (
+			[ 
+				'APP-MENU-TOGGLE-BUTTON', 
+				'BUTTON', 
+				'svg', 
+				'line'
+			].includes(tagName)
+		) { return; }
+
+		this.setState(
+			false, // hide navigation 
+			true, // show toggle button 
+			false, // collapse header to normalse size
+			true // force close navigation 
+		);
 	}
 }
