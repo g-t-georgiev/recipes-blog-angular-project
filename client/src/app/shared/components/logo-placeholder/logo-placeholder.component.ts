088991028b48826e1c04c22e1ae96454b9dcca45
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-09 20:13:10 +0200

    Created logo-placeholder component

---
+++ client/src/app/shared/components/logo-placeholder/logo-placeholder.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-logo-placeholder',
+	templateUrl: './logo-placeholder.component.html',
+	styleUrls: ['./logo-placeholder.component.css']
+})
+export class LogoPlaceholderComponent { }
