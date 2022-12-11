06fb3699c24330bb8288e9ed9bc7ebfde6f82937
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-11 18:16:27 +0200

    Deleted logo placeholder component

--- client/src/app/shared/components/logo-placeholder/logo-placeholder.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-logo-placeholder',
-	templateUrl: './logo-placeholder.component.html',
-	styleUrls: ['./logo-placeholder.component.css']
-})
-export class LogoPlaceholderComponent { }
