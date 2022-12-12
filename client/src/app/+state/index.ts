791ce01463135e0b111c85eb2af902be4e97de38
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-12 18:58:01 +0200

    Updated root state interface with the currentUser property of type IUser

--- client/src/app/+state/index.ts
+++ client/src/app/+state/index.ts
@@ -1,5 +1,9 @@
+import { IUser } from '../shared/interfaces';
+
 export interface IRootState {
     darkModeOn: boolean;
+    currentUser: IUser | null;
 };
 
 export * as theme from './dark-mode';
+export * as currentUser from './current-user';
