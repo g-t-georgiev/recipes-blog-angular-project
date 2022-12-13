52df45b2e8d5e53a48d53b38790ed48e74366592
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-13 19:15:24 +0200

    Added sign-up component in module exports list

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AuthRoutingModule } from './auth-routing.module';
+
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 
 
 @NgModule({
 	declarations: [
-    	SignInComponent
+    	SignInComponent,
+     	SignUpComponent
   ],
 	imports: [
 		CommonModule,
 		AuthRoutingModule
 	],
 	exports: [
-		SignInComponent
+		SignInComponent, 
+		SignUpComponent
 	],
 	providers: []
 })
