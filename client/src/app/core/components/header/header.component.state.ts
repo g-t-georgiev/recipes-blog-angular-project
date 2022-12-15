688723005b919575106d48d484fb058c7163c6e1
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-15 23:23:32 +0200

    Removed unnecessary imorts

--- client/src/app/core/components/header/header.component.state.ts
+++ client/src/app/core/components/header/header.component.state.ts
@@ -6,13 +6,14 @@ import { ComponentStore } from '@ngrx/component-store';
 
 export interface ILocalState {
     showNavigation: boolean,
-    showMenuButton: boolean
+    showMenuButton: boolean,
+    signingOut: boolean
 }
 
 const initialState: ILocalState = {
     showNavigation: true,
-    showMenuButton: false
-
+    showMenuButton: false,
+    signingOut: false
 };
 
 @Injectable()
@@ -22,17 +23,13 @@ export class HeaderComponentState extends ComponentStore<ILocalState> {
         super(initialState);
     }
 
-    // selectors 
-
     readonly localState$: Observable<ILocalState> = this.select((state) => state);
-
     readonly showNavigation$: Observable<boolean> = this.select(({ showNavigation }) => showNavigation);
-
-    // updaters 
+    readonly signingOut$: Observable<boolean> = this.select(({ signingOut }) => signingOut);
 
     readonly updateNavigationState = this.updater((state: ILocalState, showNavigation: boolean) => ({ ...state, showNavigation }));
-
     readonly updateMenuBtnToggleState = this.updater((state: ILocalState, showMenuButton: boolean) => ({ ...state, showMenuButton }));
+    readonly updateSignoutStatus = this.updater((state: ILocalState, signingOut: boolean) => ({ ...state, signingOut }));
 
     // effects 
 
