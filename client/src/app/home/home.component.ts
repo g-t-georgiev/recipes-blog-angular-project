6610dcce178e11f5e99c41f64c064d8a6b7a0b46
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-15 23:24:19 +0200

    Visualize current user

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { IRootState } from '../+state';
+import { IUser } from '../shared/interfaces';
+
+
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent { 
+
+	readonly currentUser$: Observable<IUser | null> = this.state.select((state) => state.currentUser)
+
+	constructor(
+		private readonly state: Store<IRootState>
+	) { }
+
+}
