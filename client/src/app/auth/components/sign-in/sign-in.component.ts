c36fcc58d577bc97fac01e3007112eda765d476a
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-16 19:14:08 +0200

    Deleted submit handler login processing logic for success/error outcome,invoked CompoentState#onLoginEventEffect instead

--- client/src/app/auth/components/sign-in/sign-in.component.ts
+++ client/src/app/auth/components/sign-in/sign-in.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs';
 
 import { ILocalState, SignInComponentState } from './sign-in.component.state';
-import { AuthService, IUserSignInDto } from '../../../core/services/auth.service';
+import { IUserSignInDto } from '../../../core/services/auth.service';
 
 
 @Component({
@@ -19,9 +17,7 @@ export class SignInComponent {
 	readonly localState$: Observable<ILocalState> = this.componentState.localState$;
 
 	constructor(
-		private readonly componentState: SignInComponentState, 
-		private readonly authService: AuthService,
-		private readonly router: Router
+		private readonly componentState: SignInComponentState
 	) { }
 
 	submitHandler(formModel: NgForm) {
@@ -33,29 +29,7 @@ export class SignInComponent {
 		console.log(formModel);
 		const userData: IUserSignInDto = formModel.form.value;
 
-		this.authService.login$(userData).pipe(
-			tap(() => {
-				this.componentState.updateProcessingState(true);
-			})
-		).subscribe({
-			next: ({ message }) => {
-				console.log(message);
-
-				this.componentState.updateMessageState(message);
-				this.componentState.updateProcessingState(false);
-				setTimeout(() => {
-					this.router.navigate(['/']);
-				}, 2e3);
-			},
-			complete: () => {
-				console.log('Login completed');
-			},
-			error: ({ error }) => {
-				console.log(error);
-				this.componentState.updateMessageState(error?.message ?? 'Something went wrong');
-				this.componentState.updateProcessingState(false);
-			}
-		});
+		this.componentState.onLoginEventEffect(userData);
 	}
 
 }
