458bf280dda14bd30506e29e011a7f57a0ba014c
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-17 18:06:42 +0200

    Defined configuration logic and behaviour on interaction

--- client/src/app/shared/components/file-upload/file-upload.component.ts
+++ client/src/app/shared/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-file-upload',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
 	styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent {
+
+	fileName: string = 'No file selected yet';
+	@Input('label') title: string = 'Choose files to upload';
+	@Input('accept') allowedFilesList!: string;
+	@ViewChild('fileUploadInput') readonly fieUploadInput!: ElementRef<HTMLInputElement>
+
+	handleFileUploadBtnClick() {
+		this.fieUploadInput.nativeElement.click();
+	}
+
+	handleFileSelectChange(ev: Event) {
+		const fileUploadInput = ev.currentTarget as HTMLInputElement;
+		const file: File | null = fileUploadInput.files?.[0] ?? null;
+
+		if (file) {
+			this.fileName = file.name;
+		}
+
+	}
 
 }
