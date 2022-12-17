f175375d8d1ca8364192ed8d76bfc66d0575d384
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-17 17:59:09 +0200

    Imported spread and remove-dots pipe

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { SpreadPipe, RemoveDotsPipe } from './pipes';
 
 
 @NgModule({
 	declarations: [
-    	FileUploadComponent
+    	FileUploadComponent,
+     	SpreadPipe,
+      	RemoveDotsPipe
   ],
 	imports: [
 		CommonModule,
 	],
 	exports: [
-		FileUploadComponent
+		FileUploadComponent, 
+		SpreadPipe, 
+		RemoveDotsPipe
 	]
 })
 export class SharedModule { }
