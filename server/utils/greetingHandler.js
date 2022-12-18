c0ef89fdd69372bdf94a8ef13d9b1f0aac1f0d12
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-18 23:18:33 +0200

    Deleted greetings handler

--- server/utils/greetingHandler.js
+++
@@ -1,8 +0,0 @@
-export function greet(username = 'stranger') {
-    const currentTime = new Date().getHours();
-    return currentTime <= 12 
-        ? `Good morning, ${username}!` 
-        : currentTime <= 18
-        ? `Good afternoon, ${username}!`
-        : `Good evening, ${username}!`;
-}
