8a66a726b5484acd9ba3d587c4893426644ca2f4
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-18 23:21:09 +0200

    Defined formidable options variable to be passed to the middleware

--- server/router/index.js
+++ server/router/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import formidable from 'express-formidable';
 
 const { default: users } = await import('./users.js');
@@ -7,9 +8,15 @@ const { default: posts } = await import('./posts.js');
 
 const { authController } = await import('../controllers/index.js');
 
+const formidableOptions = { 
+    multiples: false, // parse files as an object instead of a list
+    maxFieldsSize: 100 * 1024 * 1024, // limit file size to 10MB
+    uploadDir: path.resolve('public', 'files') // change default temp upload folder
+};
+
 const router = express.Router();
 
-router.post('/register', formidable({ multiples: false }), authController.register);
+router.post('/register', formidable(formidableOptions), authController.register);
 router.post('/login', authController.login);
 router.delete('/logout', authController.logout);
 
