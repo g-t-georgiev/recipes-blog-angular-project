f21e3d5e2c682342f840b67b33ba6565d14bc5af
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-18 15:18:17 +0200

    Integrated component with the local state service

--- client/src/app/auth/components/sign-up/sign-up.component.ts
+++ client/src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { IUserSignUpDto } from 'src/app/core/services';
+import { SignUpComponentState, ILocalState } from './sign-up.component.state';
 
 @Component({
 	selector: 'app-sign-up',
 	templateUrl: './sign-up.component.html',
-	styleUrls: ['./sign-up.component.css']
+	styleUrls: ['./sign-up.component.css'],
+	providers: [SignUpComponentState]
 })
 export class SignUpComponent {
+
+	readonly localState$: Observable<ILocalState> = this.componentState.localState$;
+
+	constructor(
+		private readonly componentState: SignUpComponentState
+	) { }
 
 	submitHandler(formModel: NgForm) {
 
@@ -14,7 +25,25 @@ export class SignUpComponent {
 			return;
 		}
 
-		console.log(formModel.form.value);
+		console.log(formModel);
+		const { email, username, profilePicture, password, repeatPassword } = formModel.form.value;
+		const userData: IUserSignUpDto = { email, username, profilePicture, password, repeatPassword };
+
+		this.componentState.onRegisterEventEffect(userData);
+	}
+
+	hasEmptyFields(formModel: NgForm) {
+	
+		for (let controlName in formModel.controls) {
+			const formControl = formModel.controls[controlName];
+			const hasRequiredError = Boolean(formControl.errors?.['required']);
+
+			if (hasRequiredError) {
+				return hasRequiredError;
+			}
+		}
+
+		return false;
 	}
 
 }
