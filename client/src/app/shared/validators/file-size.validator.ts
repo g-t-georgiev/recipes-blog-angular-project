75d140f682949404c0e68e8f0d3df5229d4b4d65
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-20 14:34:23 +0200

    Added utility function to convert the limit size to bytes

--- client/src/app/shared/validators/file-size.validator.ts
+++ client/src/app/shared/validators/file-size.validator.ts
@@ -1,5 +1,13 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
+function converToBytes(value: number): number {
+    if (value < 0) {
+        throw new Error('Value cannot be negative');
+    }
+
+    return value * 1024 * 1024;
+}
+
 /**
  * Accepts file size limit and return a validator function
  * @param limit File size in megabytes
@@ -10,6 +18,7 @@ export function maxSizeValidator(limit: number): ValidatorFn {
             return null;
         }
 
+        limit = converToBytes(limit);
         return control.value.size > limit ? { maxsize: true } : null;
     }
 }
