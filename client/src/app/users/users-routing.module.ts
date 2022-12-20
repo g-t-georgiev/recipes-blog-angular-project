6ad806f43f5644efb1ea8277cf02544124d7ea02
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-21 01:05:47 +0200

    Created users-routing module

---
+++ client/src/app/users/users-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards';
+
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        canActivate: [AuthGuard],
+        component: ProfileComponent
+    },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class UsersRoutingModule { }
