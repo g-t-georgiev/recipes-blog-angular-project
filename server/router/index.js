e35fc9f5e8cae5ee81eb0b15d99bad82003225a1
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-20 10:05:16 +0200

    Updated import statement paths for child routers

--- server/router/index.js
+++ server/router/index.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import path from 'path';
 import formidable from 'express-formidable';
 
-const { default: users } = await import('./users.js');
-const { default: themes } = await import('./themes.js');
-const { default: posts } = await import('./posts.js');
+import { formidableOptions } from '../config/formidable.config.js';
+
+import users from './users.router.js';
+import themes from './themes.router.js';
+import posts from './posts.router.js';
 
 const { authController } = await import('../controllers/index.js');
-
-const formidableOptions = { 
-    multiples: false, // parse files as an object instead of a list
-    maxFieldsSize: 100 * 1024 * 1024, // limit file size to 10MB
-    uploadDir: path.resolve('public', 'files') // change default temp upload folder
-};
 
 const router = express.Router();
 
