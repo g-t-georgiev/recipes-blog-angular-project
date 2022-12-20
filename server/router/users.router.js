11737affd2b5d13366f5979d8a15829025fa5487
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-20 13:29:59 +0200

    Refactored auth path to check and refered to the new action from users controller

--- server/router/users.router.js
+++ server/router/users.router.js
@@ -3,15 +3,13 @@ import express from 'express';
 import { authMiddleware } from '../middlewares/index.js';
 import { usersController } from '../controllers/index.js';
 
-import { authRouter } from './auth.router.js';
+export const usersRouter = express.Router({ mergeParams: true, strict: true });
 
-export const usersRouter = express.Router();
+usersRouter.get('/check', authMiddleware(false), usersController.duplicateCrendetialsCheck);
 
-usersRouter.use('/auth', authRouter);
-usersRouter
 usersRouter.get('/profile', authMiddleware(), usersController.getProfileInfo);
 usersRouter.put('/profile', authMiddleware(), usersController.editProfileInfo);
-usersRouter
+
 usersRouter.get('/themes', authMiddleware(), usersController.getUserThemes);
 usersRouter.get('/posts', authMiddleware(), usersController.getUserPosts);
 usersRouter.get('/subscriptions', authMiddleware(), usersController.getUserSubscriptions);
