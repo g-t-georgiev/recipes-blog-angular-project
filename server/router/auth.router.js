3aa21aaeb0d7b17338ffe0e2ecd7a5c81026d7f6
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-20 09:49:58 +0200

    Created auth router module for authentication checks

---
+++ server/router/auth.router.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { authMiddleware } from '../middlewares/index.js';
+import { usersController } from '../controllers/index.js';
+
+
+const router = express.Router({ mergeParams: true, strict: true });
+
+router.get('/', authMiddleware(false), usersController.authenticate);
+router.get('/email', authMiddleware(false), usersController.authenticate);
+router.get('/username', authMiddleware(false), usersController.authenticate);
