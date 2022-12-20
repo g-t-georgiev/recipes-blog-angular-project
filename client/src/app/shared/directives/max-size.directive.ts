313356ccdc087f5f14b3dc3e096f5c2b487cfb51
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-20 14:27:22 +0200

    Registered as NG_VALIDATOR and integrated with max file size validator

--- client/src/app/shared/directives/max-size.directive.ts
+++ client/src/app/shared/directives/max-size.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { maxSizeValidator } from '../validators';
 
 @Directive({
-	selector: '[appMaxSize]'
+	selector: '[maxSize]',
+	providers: [
+		{
+			provide: NG_VALIDATORS,
+			useExisting: forwardRef(() => MaxSizeDirective),
+			multi: true
+		}
+	]
 })
-export class MaxSizeDirective {
+export class MaxSizeDirective implements Validator {
+
+	@Input('maxSize') limit: number = 5;
 
 	constructor() { }
+
+	validate(control: AbstractControl<any, any>): ValidationErrors | null {
+		return maxSizeValidator(this.limit)(control);
+	}
 
 }
