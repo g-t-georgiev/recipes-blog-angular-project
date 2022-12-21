4e369626020f33d87d02a88255d2da49fdb5b021
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-21 02:10:17 +0200

    Changed max field size option to 5MB

--- server/config/formidable.config.js
+++ server/config/formidable.config.js
@@ -2,6 +2,6 @@ import path from 'path';
 
 export const formidableOptions = { 
     multiples: false, // parse files as an object instead of a list
-    maxFieldsSize: 100 * 1024 * 1024, // limit file size to 10MB
+    maxFieldsSize: 5 * 1024 * 1024, // limit file size to 5MB
     uploadDir: path.resolve('public', 'files') // change default temp upload folder
 };
