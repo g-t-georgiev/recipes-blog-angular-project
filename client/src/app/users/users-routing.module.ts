c23d121c0217e394f7ec7b174ec8340315dbf6d3
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-21 17:24:43 +0200

    Added child route to profile path referencing profile details component

--- client/src/app/users/users-routing.module.ts
+++ client/src/app/users/users-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../core/guards';
+import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
 
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
     {
         path: '',
-        pathMatch: 'full',
         canActivate: [AuthGuard],
         component: ProfileComponent,
-        title: 'Profile'
+        title: 'Profile',
+        children: [
+            {
+                path: ':id',
+                component: ProfileDetailsComponent,
+                title: 'Profile Details'
+            }
+        ]
     },
 
 ];
