8a00a35bc4d840c10e7b6eddbe0bb42a10835c43
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-21 17:23:43 +0200

    Added load profile details method making API GET request for profile details

--- client/src/app/core/services/users.service.ts
+++ client/src/app/core/services/users.service.ts
@@ -1,8 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { IUser } from 'src/app/shared/interfaces';
 import { environment } from '../../../environments/environment';
+
+
+interface ProfileDetailsApiResponse {
+	user?: IUser,
+	message: string;
+}
 
 @Injectable()
 export class UsersService {
@@ -15,6 +23,10 @@ export class UsersService {
 		const httpParams = { credentials };
 		const httpOptions = { params: httpParams };
 		return this.http.get<boolean>(`${environment.apiUrl}/users/check`, httpOptions);
+	}
+
+	loadProfileDetails() {
+		return this.http.get<ProfileDetailsApiResponse>(`${environment.apiUrl}/users/profile`);
 	}
 
 }
