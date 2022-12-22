4531c58c0f594a45337c8f0ad4de274d46759e99
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 19:12:03 +0200

    Updated recipes controller and router imports

--- server/router/index.js
+++ server/router/index.js
@@ -4,7 +4,7 @@ import formidable from 'express-formidable';
 import { formidableOptions } from '../config/formidable.config.js';
 
 import { usersRouter } from './users.router.js';
-import { themesRouter } from './themes.router.js';
+import { recipesRouter } from './recipes.router.js';
 import { postsRouter } from './posts.router.js';
 
 const { authController } = await import('../controllers/index.js');
@@ -16,7 +16,7 @@ router.post('/login', authController.login);
 router.delete('/logout', authController.logout);
 
 router.use('/users', usersRouter);
-router.use('/themes', themesRouter);
+router.use('/recipes', recipesRouter);
 router.use('/posts', postsRouter);
 
 export default router;
