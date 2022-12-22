460aa807d206a88fc4d6fe7132ee8be16c4b368f
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 06:23:59 +0200

    Added formidable middleware on edit profile path

--- server/router/users.router.js
+++ server/router/users.router.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import formidable from 'express-formidable';
 
 import { authMiddleware } from '../middlewares/index.js';
 import { usersController } from '../controllers/index.js';
+
+import { formidableOptions } from '../config/formidable.config.js';
 
 export const usersRouter = express.Router({ mergeParams: true, strict: true });
 
 usersRouter.get('/check', authMiddleware(false), usersController.duplicateCrendetialsCheck);
 
 usersRouter.get('/profile', authMiddleware(), usersController.getProfileInfo);
-usersRouter.put('/profile', authMiddleware(), usersController.editProfileInfo);
+usersRouter.put('/profile', authMiddleware(), formidable(formidableOptions), usersController.editProfileInfo);
 
 usersRouter.get('/themes', authMiddleware(), usersController.getUserThemes);
 usersRouter.get('/posts', authMiddleware(), usersController.getUserPosts);
