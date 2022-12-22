8e9ed2fa4f2f46aedaee2f162dfdd08c03e5fac9
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 22:50:12 +0200

    Added method querying for all recipes from the API

--- client/src/app/core/services/recipes.service.ts
+++ client/src/app/core/services/recipes.service.ts
@@ -1,8 +1,25 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { IRecipe } from "src/app/shared/interfaces";
+
+import { environment } from "src/environments/environment";
+
+export interface IRecipesQueryResponse {
+    recipes: IRecipe[] | [];
+    message: string;
+    total: number;
+}
 
 @Injectable()
 export class RecipesService {
 
-    constructor() { }
+    constructor(
+        private readonly http: HttpClient
+    ) { }
+
+    getAll(page: number, size: number, filterOptions: { [key: string]: string } = {}): Observable<IRecipesQueryResponse> {
+        return this.http.get<IRecipesQueryResponse>(environment.apiUrl, { params: { page, size, ...filterOptions }});
+    }
 
 }
