1ebf107676f102c5844076b83f55ff71f863544a
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 22:51:33 +0200

    Added recipes list component state  service as a local provider

--- client/src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RecipeListComponentState } from './recipe-list.component.state';
 
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html',
-	styleUrls: ['./recipe-list.component.css']
+	styleUrls: ['./recipe-list.component.css'],
+	providers: [RecipeListComponentState]
 })
 export class RecipeListComponent {
 
