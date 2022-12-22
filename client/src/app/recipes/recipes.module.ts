c2de33e477ecec76a5c539bf4d7754dcd4c65fed
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 21:53:45 +0200

    Registered recipes list component in module's declarations and exports

--- client/src/app/recipes/recipes.module.ts
+++ client/src/app/recipes/recipes.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipeListComponent } from './components';
 
 
 @NgModule({
-	declarations: [],
+	declarations: [
+    	RecipeListComponent
+  ],
 	imports: [
 		CommonModule,
 		RecipesRoutingModule
+	],
+	exports: [
+		RecipeListComponent
 	]
 })
 export class FeatureModule { }
