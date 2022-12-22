897b5929ffc6d0650fc746d66545aa650dcdf485
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-23 00:15:21 +0200

    Added recipes route

--- client/src/app/recipes/recipes-routing.module.ts
+++ client/src/app/recipes/recipes-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RecipeListComponent } from './components';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: RecipeListComponent
+    }
+];
 
 @NgModule({
     imports: [
