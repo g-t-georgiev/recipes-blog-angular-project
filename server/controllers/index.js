94afb3159de57f003b378d00533961a0563ad0c2
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-22 19:11:26 +0200

    Renamed export entity name from themesController to recipesController

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
 export * as authController from './auth.controller.js';
-export * as themesController from './theme.controller.js';
+export * as recipesController from './recipes.controller.js';
 export * as postsController from './post.controller.js';
 export * as usersController from './user.controller.js';
 export * as likesController from './like.controller.js';
