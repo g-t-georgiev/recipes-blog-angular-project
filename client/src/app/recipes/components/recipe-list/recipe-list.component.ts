a4698c228d7dd8838d06c67376002624d8a646aa
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-25 14:26:22 +0200

    Refactored: Deleted old logic for loading recipes; Integrated in onInit method the initializerEffect

--- client/src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { debounceTime, distinctUntilChanged, Subject, Subscription, switchMap, takeUntil, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Subscription, tap } from 'rxjs';
 
 import { RecipeListComponentState } from './recipe-list.component.state';
 
@@ -17,78 +16,19 @@ export class RecipeListComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	readonly localState$ = this.componentState.localState$;
 
-	@ViewChild('filterByTitleInput') filterByTitleInput!: NgForm;
+	@ViewChild('filterResultsForm') filterResultsForm!: NgForm;
 
 	constructor(
-		private readonly activatedRoute: ActivatedRoute,
-		private readonly router: Router,
 		private readonly componentState: RecipeListComponentState
 	) { }
 
 	ngOnInit(): void {
-
 		this.subscription.add(
-			this.activatedRoute.queryParams.subscribe({
-				next: (params => {
-
-					let { page, size, title } = params;
-					let updatedQueryParams;
-
-					if (!page || isNaN(+page) || +page < 1) {
-						updatedQueryParams = {
-							...params,
-							page: 1
-						}
-					}
-
-					if (!size || isNaN(+size) || +size < 1) {
-						updatedQueryParams = {
-							...params,
-							size: 10
-						}
-					}
-
-					if (updatedQueryParams) {
-						const updatedUrlTree = this.router.createUrlTree([], {
-							queryParams: updatedQueryParams,
-							relativeTo: this.activatedRoute
-						});
-
-						this.router.navigateByUrl(updatedUrlTree);
-						return;
-					}
-
-					
-					this.componentState.queryParamsChangeEffect(params);
-				})
-			})
+			this.componentState.initializerEffect()
 		);
-
 	}
 
 	ngAfterViewInit(): void {
-
-		this.filterByTitleInput.valueChanges?.pipe(
-			debounceTime(500),
-			distinctUntilChanged(), 
-			tap((title) => {
-
-				title = title?.trim();
-				console.log(title);
-
-				if (title != null) {
-					let queryParams = title.length ? { title } : null;
-
-					const updatedUrlTree = this.router.createUrlTree([], {
-						queryParams,
-						relativeTo: this.activatedRoute
-					});
-	
-					this.router.navigateByUrl(updatedUrlTree);
-				}
-
-			})
-		).subscribe();
 
 	}
 
@@ -100,38 +40,22 @@ export class RecipeListComponent implements OnInit, AfterViewInit, OnDestroy {
 		return Math.ceil(totalEntriesCount / entriesPerPageCount);
 	}
 
-	togglePreviousPage(ev: PointerEvent, currentPage: number) {
-		ev.preventDefault();
+	togglePreviousPage(currentPage: number) {
 
-		console.log('#prev', currentPage);
 		if (currentPage <= 1) {
 			return;
 		}
 
-		const updatedUrlTree = this.router.createUrlTree([], {
-			queryParams: { page: currentPage - 1, size: 1 },
-			relativeTo: this.activatedRoute
-		});
-
-		this.router.navigateByUrl(updatedUrlTree);
 	}
 
-	toggleNextPage(ev: PointerEvent, currentPage: number, entriesPerPageCount: number, totalEntriesCount: number) {
-		ev.preventDefault();
+	toggleNextPage(currentPage: number, entriesPerPageCount: number, totalEntriesCount: number) {
 
 		let totalPagesCount = this.getTotalPagesCount(entriesPerPageCount, totalEntriesCount);
 
-		console.log('#next', currentPage, totalPagesCount);
 		if (currentPage >= totalPagesCount) {
 			return;
 		}
 
-		const updatedUrlTree = this.router.createUrlTree([], {
-			queryParams: { page: currentPage + 1, size: 1 },
-			relativeTo: this.activatedRoute
-		});
-
-		this.router.navigateByUrl(updatedUrlTree);
 	}
 
 }
