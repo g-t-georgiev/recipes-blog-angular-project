5be4dd791e3c52caed900b482ff69399bee375b4
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-25 14:48:53 +0200

    Added inputs, outputs and methods base interface requirements for toggling and managing page changes

--- client/src/app/shared/components/paginator/paginator.component.ts
+++ client/src/app/shared/components/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-paginator',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
 	styleUrls: ['./paginator.component.css']
 })
 export class PaginatorComponent {
+
+	@Input('current') currentPage: number = 0;
+	@Input('total') totalPagesCount: number = 0;
+
+	@Output() goTo: EventEmitter<number> = new EventEmitter<number>();
+	@Output() next: EventEmitter<number> = new EventEmitter<number>();
+	@Output() previous: EventEmitter<number> = new EventEmitter<number>();
+
+	onGoTo(page: number): void {
+		this.goTo.emit(page);
+	}
+
+	onNext(): void {
+		this.next.emit(this.currentPage);
+	}
+
+	onPrevious(): void {
+		this.previous.emit(this.currentPage);
+	}
 
 }
