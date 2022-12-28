d4d7e2b90612ce2ddd25b98997884326535376b4
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-28 17:17:21 +0200

    Cleared boilerplate code and leave only the ngx-paginator.module exports

--- client/projects/ngx-paginator/src/public-api.ts
+++ client/projects/ngx-paginator/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-paginator
  */
 
-export * from './lib/ngx-paginator.service';
-export * from './lib/ngx-paginator.component';
 export * from './lib/ngx-paginator.module';
