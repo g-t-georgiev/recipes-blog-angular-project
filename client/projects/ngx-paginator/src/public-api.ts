e963f9500f581d2edd38633bbcc874180e6f7986
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-28 17:44:40 +0200

    Added PaginatorComponent as export; Updated PaginatorModule export path

--- client/projects/ngx-paginator/src/public-api.ts
+++ client/projects/ngx-paginator/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of ngx-paginator
  */
 
-export * from './lib/ngx-paginator.module';
+export * from './lib/paginator.module';
+export * from './lib/paginator/paginator.component';
