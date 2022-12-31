bfb8bfc0438e78d258d777739240f1c60bfcf8fe
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-31 15:42:00 +0200

    Registered link active directive in the directives directory common exports

--- client/projects/recipes-blog-app/src/app/shared/directives/index.ts
+++ client/projects/recipes-blog-app/src/app/shared/directives/index.ts
@@ -2,3 +2,4 @@ export * from './maxlength-validator.directive';
 export * from './passwords-match-validator.directive';
 export * from './duplicate-credentials-validator.directive';
 export * from './max-size.directive';
+export * from './link-active.directive';
