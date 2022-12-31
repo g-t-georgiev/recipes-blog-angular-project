a3a5c8ab361b2adf8efc41a12a6d9644f3c237e9
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2022-12-31 15:42:37 +0200

    Registered link active directive in module's declarations and exports

--- client/projects/recipes-blog-app/src/app/shared/shared.module.ts
+++ client/projects/recipes-blog-app/src/app/shared/shared.module.ts
@@ -3,7 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { FileUploadComponent, PaginatorComponent } from './components';
 import { SpreadPipe, RemoveDotsPipe } from './pipes';
-import { MaxlengthValidatorDirective, PasswordsMatchValidatorDirective, DuplicateCredentialsValidatorDirective, MaxSizeDirective } from './directives';
+
+import { 
+	MaxlengthValidatorDirective, 
+	PasswordsMatchValidatorDirective, 
+	DuplicateCredentialsValidatorDirective, 
+	MaxSizeDirective, 
+	LinkActiveDirective 
+} from './directives';
+
 import { CustomValidatorsService } from './services';
 
 
@@ -16,7 +24,8 @@ import { CustomValidatorsService } from './services';
         PasswordsMatchValidatorDirective,
         DuplicateCredentialsValidatorDirective,
         MaxSizeDirective,
-        PaginatorComponent
+        PaginatorComponent,
+        LinkActiveDirective
   ],
 	imports: [
 		CommonModule,
@@ -29,6 +38,7 @@ import { CustomValidatorsService } from './services';
 		PasswordsMatchValidatorDirective,
 		DuplicateCredentialsValidatorDirective,
 		MaxSizeDirective,
+		LinkActiveDirective,
 		PaginatorComponent
 	],
 	providers: [
